c12a915220b3e26fac9ad59221203e8568f15e4f
Andreas Rehn <rehn.andreas86@gmail.com>
2024-10-14 20:02:52 +0200

    milkv-duo: update gpio script
    to enable and switch all 4 relay states

--- milkv-duo/enable-gpios.sh
+++ milkv-duo/enable-gpios.sh
@@ -1,15 +1,35 @@
 #!/bin/sh
 # configure only Header J3
 # https://milkv.io/docs/duo/getting-started/duos#header-j3
-duo-pinmux -w B20 B20
-ceiling_heating_pump=451
+IN_1=468
+IN_2=469
+IN_3=466
+IN_4=459
 
-GPIO=/sys/class/gpio/gpio${ceiling_heating_pump}
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <1|2|3|4>"
+    exit 1
+fi
+
+duo-pinmux -w B20/B20
+duo-pinmux -w B21/B21
+duo-pinmux -w B18/B18
+duo-pinmux -w B11/B11
+
+case "$1" in
+    1) switch_pin=$IN_1 ;;
+    2) switch_pin=$IN_2 ;;
+    3) switch_pin=$IN_3 ;;
+    4) switch_pin=$IN_4 ;;
+    *) echo "Invalid option: $1. Please use 1, 2, 3, or 4." ; exit 1 ;;
+esac
+
+GPIO=/sys/class/gpio/gpio${switch_pin}
 
 if test -d ${GPIO}; then
-    echo "PIN ${ceiling_heating_pump} already exported"
+    echo "PIN ${switch_pin} already exported"
 else
-    echo ${ceiling_heating_pump} > /sys/class/gpio/export
+    echo ${switch_pin} > /sys/class/gpio/export
 fi
 
 echo out > ${GPIO}/direction
